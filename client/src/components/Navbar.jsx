1a8901ca229cb9bea581150fc9cbdeaaf0b3e0df
Isuru Adithya Sandaruwan <isuruadithyasandaruwan@gmail.com>
2025-07-10 19:54:07 +0530

    feat: update Navbar component to remove React import and add higher z-index to navigation bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
@@ -27,7 +27,7 @@ const Navbar = () => {
   }, [searchQuery]);
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all z-50">
       <NavLink to={"/"} onClick={() => setOpen(false)}>
         <img
           className="h-16 w-auto rounded-full object-contain"
